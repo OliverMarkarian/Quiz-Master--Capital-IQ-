8e8da1094bec5f3ed55ede59ec79fbfef87f3973
Oliver <oliver.markarian@gmail.com>
2024-08-09 17:33:28 +0200

    Created functionality for correct answers to quizz

--- Assets/Scripts/Quizz.cs
+++ Assets/Scripts/Quizz.cs
@@ -9,6 +9,9 @@ public class Quizz : MonoBehaviour
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
+    int correctAnswerIndex;
+    [SerializeField]Sprite defaultAnswerSprite;
+    [SerializeField]Sprite correctAnswerSprite;
     void Start()
     {
         questionText.text = question.GetQuestion();
@@ -19,12 +22,22 @@ public class Quizz : MonoBehaviour
             
         }
 
-        
-       
-        
-
-        
-
+    }    
+    public void OnAnswerSelected(int index){
+        Image buttonImage;
+        if(index == question.GetCorrectAnswerIndex())
+        {
+            questionText.text = "Correct!";
+            buttonImage = answerButtons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        } else 
+        {
+            correctAnswerIndex = question.GetCorrectAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry, the correct answer was:\n" + correctAnswer;
+            buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
     }
     
 }
