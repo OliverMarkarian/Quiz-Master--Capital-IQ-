1d2a7778d121b00fb446ac11bff8ecc328da1ea8
Oliver <oliver.markarian@gmail.com>
2024-08-09 17:56:29 +0200

    Imported a timer (not yet initialised)

--- Assets/Scripts/Quizz.cs
+++ Assets/Scripts/Quizz.cs
@@ -10,27 +10,24 @@ public class Quizz : MonoBehaviour
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
-    [SerializeField]Sprite defaultAnswerSprite;
-    [SerializeField]Sprite correctAnswerSprite;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
     void Start()
     {
-        questionText.text = question.GetQuestion();
+        //DisplayQuestion();
+        GetNextQuestion();
+    }
+    public void OnAnswerSelected(int index)
+    {
 
-        for (int i = 0; i < answerButtons.Length; i++){
-            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
-            buttonText.text = question.GetAnswer(i);
-            
-        }
-
-    }    
-    public void OnAnswerSelected(int index){
         Image buttonImage;
-        if(index == question.GetCorrectAnswerIndex())
+        if (index == question.GetCorrectAnswerIndex())
         {
             questionText.text = "Correct!";
             buttonImage = answerButtons[index].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
-        } else 
+        }
+        else
         {
             correctAnswerIndex = question.GetCorrectAnswerIndex();
             string correctAnswer = question.GetAnswer(correctAnswerIndex);
@@ -38,6 +35,39 @@ public class Quizz : MonoBehaviour
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
         }
+        SetButtonState(false);
     }
-    
+
+    void GetNextQuestion(){
+        SetButtonState(true);
+        SetDefaultButtonSprites();
+        DisplayQuestion();
+        
+    }
+
+    void DisplayQuestion()
+    {
+
+        questionText.text = question.GetQuestion();
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            TextMeshProUGUI buttonText = answerButtons[i].GetComponentInChildren<TextMeshProUGUI>();
+            buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    void SetButtonState(bool state)
+    {
+        for (int i = 0; i < answerButtons.Length; i++)
+        {
+            Button button = answerButtons[i].GetComponent<Button>();
+            button.interactable = state;
+        }
+    }
+    void SetDefaultButtonSprites(){
+        for (int i = 0; i < answerButtons.Length; i++){
+            Image buttonImage = answerButtons[i].GetComponent<Image>();
+            buttonImage.sprite = defaultAnswerSprite;
+        }
+    }
 }
